47f7ad7e0ae47a6dc63c6cceec854d1d2fd153aa
Kid <228286169@qq.com>
2025-02-24 16:16:46 +0800

    feat

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2006-2025 FacilityONE Inc. All Rights Reserved
+ *
+ * 注意：
+ * 本软件内容仅限于费哲软件内部传阅，禁止外泄以及用于其他商业目的
+ * 费哲软件(FacilityONE) : www.facilityone.cn
+ */
+
+/**
+ * description goes here.
+ *
+ * @author kid.bian
+ * @date 2025/2/24 16:16
+ * @since 1.0
+ **/public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
